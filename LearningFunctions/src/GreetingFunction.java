e22dade69d3abbd86224ec9074d5f8908e5e9b70
Tx-3011 <iamtejdeepn@gmail.com>
2023-10-15 14:00:05 +0530

    added a greeting function

--- LearningFunctions/src/GreetingFunction.java
+++ LearningFunctions/src/GreetingFunction.java
@@ -1,14 +1,18 @@
+import java.util.Scanner;
 public class GreetingFunction {
-    
+    public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = in.next();
+        String personalised = myGreet(name);
+        System.out.println(personalised);
 
+    }
 
+    private static String myGreet(String name) {
 
+        return "It's Nice To See You Here, " + name;
 
-
-
-
-
-
-
+    }
 }
