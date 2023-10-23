9633bd30838b7dafed7f08bf0b2c2ace2063ac91
Tx-3011 <iamtejdeepn@gmail.com>
2023-10-23 15:42:51 +0530

    fixed APF

--- LearningFunctions/src/com/functions/questions/ArmstrongPrintFunction.java
+++ LearningFunctions/src/com/functions/questions/ArmstrongPrintFunction.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class ArmstrongPrintFunction {
     public static void main(String[] args) {
         System.out.println("How many numbers should I check for?");
-       Scanner in = new Scanner(System.in);
-       int n = in.nextInt();
-        for (int i = 1; i <= n; i++ ){
-            if (isArmstrong(i)){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.close();
+
+        for (int i = 0; i <= n; i++) {
+            if (isArmstrong(i)) {
                 System.out.print(i + " ");
             }
         }
@@ -16,13 +18,14 @@ public class ArmstrongPrintFunction {
 
     static boolean isArmstrong(int n) {
         int original = n;
+        int numberOfDigits = String.valueOf(n).length();
         int sum = 0;
-        while (n>0){
-            int r = n%10;
-            n = n/10;
-            sum = sum + r*r*r;
-        }
-        return (sum == original);
 
+        for (int i = n; i > 0; i /= 10) {
+            int digit = i % 10;
+            sum += Math.pow(digit, numberOfDigits);
+        }
+
+        return (sum == original);
     }
 }
