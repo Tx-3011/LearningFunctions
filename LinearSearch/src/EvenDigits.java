c5c0df11ff4184347b5980c6f6480a64cf989b94
Tejdeep Nookala <iamtejdeepn@gmail.com>
2024-02-29 16:26:08 +0530

    completed even digits

--- LinearSearch/src/EvenDigits.java
+++ LinearSearch/src/EvenDigits.java
@@ -2,6 +2,42 @@ public class EvenDigits {
 
     public static void main(String[] args) {
 
+
+        int [] arr = {234,555,6535,6535,44,11};
+        System.out.println(FindNumbers(arr));
+
+
     }
+
+    static int FindNumbers(int[] nums){
+        int count = 0;
+        for (int num:nums){
+
+            if(even(num)){
+
+                count++;
+            }
+
+        }
+        return count;
+
+    }
+
+    static boolean even(int num){
+        int NumberOfDigits = digits(num);
+        return NumberOfDigits % 2 == 0;
+    }
+
+    static int digits(int num){
+        int count = 0;
+        while (num>0){
+
+            num = num/10;
+            count++;
+
+        }
+        return count;
+    }
+
 
 }
